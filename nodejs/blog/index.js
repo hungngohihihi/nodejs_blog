7a6f0bbb0bc77118cff173a9b7eaf9782596f66a
Hùng Đinh <76930315+hungngohihihi@users.noreply.github.com>
2022-12-17 00:24:42 +0700

    nodemon & debug

--- nodejs/blog/index.js
+++ nodejs/blog/index.js
@@ -2,7 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/trang-chu', (req, res) => {  // Định nghĩa tuyến đường
+app.get('/', (req, res) => {  // Định nghĩa tuyến đường
+  var a = 1;
+  var b = 2;
+
+  var c = a + b;
+
   res.send('Hello World!')
 })
 
