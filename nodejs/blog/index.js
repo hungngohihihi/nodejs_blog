d7b14a9f3273919373e93c70070169b7c06dac74
Hùng Đinh <hungngohihihi@gmail.com>
2022-12-17 01:10:33 +0700

    push for MacOS

--- nodejs/blog/index.js
+++ nodejs/blog/index.js
@@ -3,12 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {  // Định nghĩa tuyến đường
-  var a = 1;
-  var b = 2;
-
-  var c = a + b;
-
-  res.send('Hello World!')
+  res.send('Hello World!   MACOS')
 })
 
 app.listen(port, () => {
